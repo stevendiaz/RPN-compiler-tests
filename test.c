99869504664bcd86a8e110622be9a6ccb0a99e2f
Steven Diaz <stevendiaz@utexas.edu>
2015-10-28 16:26:07 -0500

    Create test.c

---
+++ test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <stdint.h>
+
+extern int64_t compute (int64_t x, int64_t y, int64_t z);
+
+void testFunction() {
+  int64_t x = 1;
+  int64_t y = 2;
+  int64_t z = 3;
+  int64_t result = compute(x,y,z);
+  printf("RESULT: %lld\n", (long long)result);
+}
+
+
+int main() {
+        testFunction();
+}
