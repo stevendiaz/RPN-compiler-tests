c4e3d61d83179ec1027d2baf7a967acbbc5fd8e2
Steven Diaz <stevendiaz@utexas.edu>
2016-04-11 11:45:29 -0500

    Takes in command line as arguments

--- test.c
+++ test.c
@@ -6,15 +6,27 @@
 
 extern int64_t compute (int64_t x, int64_t y, int64_t z);
 
-void testFunction() {
-  int64_t x = 1;
-  int64_t y = 2;
-  int64_t z = 3;
+void testFunction(int64_t x, int64_t y, int64_t z) {
   int64_t result = compute(x,y,z);
   printf("RESULT: %lld\n", (long long)result);
 }
 
 
-int main() {
-  testFunction();
+int main(int argc, char *argv[]) {
+        int64_t x;
+        int64_t y;
+        int64_t z;
+        if(argc != 3) {
+          x = 70;
+          y = 80;
+          z = 90;
+          printf("Invalid arguments: must be 3 integers\n");
+          printf("Setting x,y,z to default...");
+        } else {
+          x = atoi(argv[1]);
+          y = atoi(argv[2]);
+          z = atoi(argv[3]);
+        }
+        testFunction(x,y,z);
+        return 0;
 }
